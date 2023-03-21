7f331ecce19ea61ac804250365f7c2412a0a2b48
小白熊 <mclouds@126.com>
2023-03-22 02:55:34 +0800

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,6 +17,13 @@ async function bootstrap() {
         defaultVersion: '1',
     });
     app.setGlobalPrefix('api');
+
+    // const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
+    // const document = SwaggerModule.createDocument(app, config, {
+    //     ignoreGlobalPrefix: true,
+    // });
+    // SwaggerModule.setup('api', app, document);
+
     await app.listen(+configService.get('APP_PORT'));
 }
 bootstrap();
