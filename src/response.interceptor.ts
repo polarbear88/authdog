b9e8a6f987f94e438476f0c5fcd9f3b8d61a0a6d
小白熊 <mclouds@126.com>
2023-04-07 20:11:08 +0800

    Update response.interceptor.ts

--- src/response.interceptor.ts
+++ src/response.interceptor.ts
@@ -3,11 +3,14 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ExpressUtils } from './common/utils/express.utils';
 import { CryptoUtils } from './common/utils/crypyo.utils';
+import { Response } from 'express';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const request = context.switchToHttp().getRequest();
+        // 强制设置响应状态码为200
+        (context.switchToHttp().getResponse() as Response).status(200);
         // 获取是否是api请求是否需要加密响应
         const isEncrypt = !!request['is_need_encrypt_res'];
         // 修改响应内容以封装和加入基础数据
