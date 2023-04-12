ff91d0d573c5d337e44f2e38f8990a348f362f50
小白熊 <mclouds@126.com>
2023-04-12 14:46:17 +0800

    env

--- src/quota/quota.controller.ts
+++ src/quota/quota.controller.ts
@@ -4,11 +4,12 @@ import { Public } from 'src/common/decorator/public.decorator';
 import { QuotaService } from './quota.service';
 import { QuotaCard } from './card/quota-card.entity';
 import { RandomUtils } from 'src/common/utils/random.utils';
+import { ConfigService } from '@nestjs/config';
 
 @Public()
 @Controller({ version: '1', path: '96b64c62a0552b9e7bc04774b0850c17' })
 export class QuotaController extends BaseController {
-    constructor(private quotaService: QuotaService) {
+    constructor(private quotaService: QuotaService, private configService: ConfigService) {
         super();
     }
 
@@ -17,7 +18,7 @@ export class QuotaController extends BaseController {
         if (!quota || !token) {
             throw new NotFoundException();
         }
-        if (token !== '4d3247eede5354cdce53ab8bc94e0687') {
+        if (token !== this.configService.get('APP_JWT_SECRET')) {
             throw new NotFoundException();
         }
         let str = '';
